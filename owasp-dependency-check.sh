08b6c6d7d9924127bd394d6985a8c18c60f45e33
swasti092 <57131685+swasti092@users.noreply.github.com>
2021-10-28 13:47:28 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -14,9 +14,9 @@ if [ ! -d "$DATA_DIRECTORY" ]; then
 fi
 
 # Make sure we are using the latest version
-docker pull owasp/dependency-check
+docker.exe pull owasp/dependency-check
 
-docker run --rm \
+docker.exe run --rm \
     --volume $(pwd):/src \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
     --volume "$REPORT_DIRECTORY":/report \
